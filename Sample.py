67519b4883ca41bace88dcdde546ecc41d84866a
aliu139 <a.liu@hotmail.com>
2016-04-02 10:37:07 -0400

    sdfdsf

--- Sample.py
+++ Sample.py
@@ -8,6 +8,7 @@
 # between Leap Motion and you, your company or other organization.             #
 ################################################################################
 import Leap, sys, thread, time
+import subprocess
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
 
@@ -15,6 +16,8 @@ class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
     bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+
+    gripped = False
 
     def on_init(self, controller):
         print "Initialized"
@@ -39,57 +42,73 @@ class SampleListener(Leap.Listener):
         # Get the most recent frame and report some basic information
         frame = controller.frame()
 
-        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
-              frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
+        global gripped
+
+        # print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
+        #       frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
 
         # Get hands
         for hand in frame.hands:
-
             handType = "Left hand" if hand.is_left else "Right hand"
 
-            print "  %s, id %d, position: %s" % (
-                handType, hand.id, hand.palm_position)
+            # print "  %s, id %d, position: %s" % (
+            #     handType, hand.id, hand.palm_position)
 
             # Get the hand's normal vector and direction
             normal = hand.palm_normal
             direction = hand.direction
 
+            if(hand.grab_strength > 0.8 and not gripped):
+                gripped = True
+                print("Grip")
+                response = raw_input('Please enter your commit message: ')
+                r = str(response)
+                print r
+                subprocess.call(["git", "commit", "-m", r])
+
+            if(hand.grab_strength < 0.2 and gripped):
+                gripped = False
+                print("Open")
+                subprocess.call(["git", "add", "-u"])
+
             # Calculate the hand's pitch, roll, and yaw angles
-            print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
-                direction.pitch * Leap.RAD_TO_DEG,
-                normal.roll * Leap.RAD_TO_DEG,
-                direction.yaw * Leap.RAD_TO_DEG)
+            # print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
+            #     direction.pitch * Leap.RAD_TO_DEG,
+            #     normal.roll * Leap.RAD_TO_DEG,
+            #     direction.yaw * Leap.RAD_TO_DEG)
 
             # Get arm bone
             arm = hand.arm
-            print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
-                arm.direction,
-                arm.wrist_position,
-                arm.elbow_position)
+            # print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
+            #     arm.direction,
+            #     arm.wrist_position,
+            #     arm.elbow_position)
 
             # Get fingers
-            for finger in hand.fingers:
+            # for finger in hand.fingers:
 
-                print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
-                    self.finger_names[finger.type],
-                    finger.id,
-                    finger.length,
-                    finger.width)
+            #     print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
+            #         self.finger_names[finger.type],
+            #         finger.id,
+            #         finger.length,
+            #         finger.width)
 
-                # Get bones
-                for b in range(0, 4):
-                    bone = finger.bone(b)
-                    print "      Bone: %s, start: %s, end: %s, direction: %s" % (
-                        self.bone_names[bone.type],
-                        bone.prev_joint,
-                        bone.next_joint,
-                        bone.direction)
+            #     # Get bones
+            #     for b in range(0, 4):
+            #         bone = finger.bone(b)
+            #         print "      Bone: %s, start: %s, end: %s, direction: %s" % (
+            #             self.bone_names[bone.type],
+            #             bone.prev_joint,
+            #             bone.next_joint,
+            #             bone.direction)
 
         # Get tools
-        for tool in frame.tools:
+        # for tool in frame.tools:
 
-            print "  Tool id: %d, position: %s, direction: %s" % (
-                tool.id, tool.tip_position, tool.direction)
+        #     print "  Tool id: %d, position: %s, direction: %s" % (
+        #         tool.id, tool.tip_position, tool.direction)
+
+        circled = False
 
         # Get gestures
         for gesture in frame.gestures():
@@ -108,9 +127,13 @@ class SampleListener(Leap.Listener):
                     previous_update = CircleGesture(controller.frame(1).gesture(circle.id))
                     swept_angle =  (circle.progress - previous_update.progress) * 2 * Leap.PI
 
-                print "  Circle id: %d, %s, progress: %f, radius: %f, angle: %f degrees, %s" % (
-                        gesture.id, self.state_names[gesture.state],
-                        circle.progress, circle.radius, swept_angle * Leap.RAD_TO_DEG, clockwiseness)
+                # print "  Circle id: %d, %s, progress: %f, radius: %f, angle: %f degrees, %s" % (
+                #         gesture.id, self.state_names[gesture.state],
+                #         circle.progress, circle.radius, swept_angle * Leap.RAD_TO_DEG, clockwiseness)
+
+                if(circle.progress > 0.9 and circle.progress < 1.0 and not circled):
+                    circled = True
+                    subprocess.call(["git", "status"])
 
             if gesture.type == Leap.Gesture.TYPE_SWIPE:
                 swipe = SwipeGesture(gesture)
@@ -130,8 +153,9 @@ class SampleListener(Leap.Listener):
                         gesture.id, self.state_names[gesture.state],
                         screentap.position, screentap.direction )
 
-        if not (frame.hands.is_empty and frame.gestures().is_empty):
-            print ""
+
+        # if not (frame.hands.is_empty and frame.gestures().is_empty):
+        #     print ""
 
     def state_string(self, state):
         if state == Leap.Gesture.STATE_START:
@@ -150,6 +174,9 @@ def main():
     # Create a sample listener and controller
     listener = SampleListener()
     controller = Leap.Controller()
+
+    global gripped
+    gripped = True
 
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
